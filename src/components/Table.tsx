30c0c8be91b39696af75e2e2e046040e3da195fb
tymino <d_team-lex@bk.ru>
2022-01-15 14:55:16 +0300

    init

---
+++ src/components/Table.tsx
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+const Table = () => {
+  return <div className="table">react</div>;
+};
+
+export default Table;
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import './sass/index.sass';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Table from './components/Table';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Table />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
