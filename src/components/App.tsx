909b54b5ba574ff0fe5612766ccb1b1aeafe5d62
tymino <d_team-lex@bk.ru>
2022-01-16 17:58:54 +0300

    save

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,6 +7,21 @@ const tableData: IData[] = [
   { id: 0, date: '12-01-2022', title: 'Box', amount: 108, distance: 0.6 },
   { id: 1, date: '04-01-2022', title: 'Book', amount: 214, distance: 2.2 },
   { id: 2, date: '30-12-2021', title: 'Pan', amount: 340, distance: 1.1 },
+  { id: 3, date: '12-01-2022', title: 'Box', amount: 108, distance: 0.6 },
+  { id: 4, date: '04-01-2022', title: 'Book', amount: 214, distance: 2.2 },
+  { id: 5, date: '30-12-2021', title: 'Pan', amount: 340, distance: 1.1 },
+  { id: 6, date: '12-01-2022', title: 'Box', amount: 108, distance: 0.6 },
+  { id: 7, date: '04-01-2022', title: 'Book', amount: 214, distance: 2.2 },
+  { id: 8, date: '30-12-2021', title: 'Pan', amount: 340, distance: 1.1 },
+  { id: 9, date: '12-01-2022', title: 'Box', amount: 108, distance: 0.6 },
+  { id: 10, date: '04-01-2022', title: 'Book', amount: 214, distance: 2.2 },
+  { id: 11, date: '30-12-2021', title: 'Pan', amount: 340, distance: 1.1 },
+  { id: 12, date: '12-01-2022', title: 'Box', amount: 108, distance: 0.6 },
+  { id: 13, date: '04-01-2022', title: 'Book', amount: 214, distance: 2.2 },
+  { id: 14, date: '30-12-2021', title: 'Pan', amount: 340, distance: 1.1 },
+  { id: 15, date: '12-01-2022', title: 'Box', amount: 108, distance: 0.6 },
+  { id: 16, date: '04-01-2022', title: 'Book', amount: 214, distance: 2.2 },
+  { id: 17, date: '30-12-2021', title: 'Pan', amount: 340, distance: 1.1 },
 ];
 
 const App = () => {
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -8,9 +8,58 @@ interface ITableProps {
 }
 
 const Table: React.FC<ITableProps> = ({ colName, list }) => {
+  const conditions: string[] = ['---', 'равно', 'содержит', 'больше', 'меньше'];
+  const [selectColumnName, setSelectColumnName] = React.useState<string>(colName[0]);
+
+  const [selectСondition, setSelectСondition] = React.useState<string | undefined>(conditions[0]);
+  const [inputSearch, setInputSearch] = React.useState<string>('');
+
+  const [pagination, setPagination] = React.useState<any>({
+    currentPage: 1,
+    visibleItemsPerPage: 3,
+    allPages: Math.ceil(list.length / 3),
+  });
+
+  const [actualTableData, setActualTableData] = React.useState<IData[]>(list);
+
+  const handleSelectColumnName = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectColumnName(event.target.value);
+  };
+
+  const handleSelectСondition = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectСondition(event.target.value);
+  };
+
+  const handleInputSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputSearch(event.target.value);
+  };
+
   return (
     <div className="table-component">
-      {list.length > 0 && (
+      <div className="table-component__navigation">
+        <div className="navigation__filter-column">
+          <select value={selectColumnName} onChange={handleSelectColumnName}>
+            {colName.slice(1).map((name: string, index) => (
+              <option key={name + index} value={name}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="navigation__filter-condition">
+          <select value={selectСondition} onChange={handleSelectСondition}>
+            {conditions.map((condition: string, index) => (
+              <option key={condition + index} value={condition}>
+                {condition}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="navigation__filter-search">
+          <input type="text" value={inputSearch} onChange={handleInputSearch} />
+        </div>
+      </div>
+      {actualTableData.length > 0 && (
         <table className="table-component__table" cellSpacing="0">
           <thead className="table-component__thead">
             <tr className="table-component__tr">
@@ -22,7 +71,7 @@ const Table: React.FC<ITableProps> = ({ colName, list }) => {
             </tr>
           </thead>
           <tbody className="table-component__tbody">
-            {list.map((item: IData) => (
+            {actualTableData.map((item: IData) => (
               <tr className="table-component__tr" key={item.id}>
                 {(Object.keys(item) as Array<keyof typeof item>).map((value, index) => {
                   if (/id/gi.test(value)) return null;
